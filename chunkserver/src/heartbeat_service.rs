6d7a814b827b63ff243d2e861813121b289f4e09
dev-lew <55928726+dev-lew@users.noreply.github.com>
2026-07-25 22:04:39 -0400

    Add lease assignment logic

--- chunkserver/src/heartbeat_service.rs
+++ chunkserver/src/heartbeat_service.rs
@@ -1,7 +1,10 @@
-use std::collections::{HashMap, HashSet};
+use std::fs;
+use std::fs::File;
 use std::path::PathBuf;
-use std::time::{Duration, Instant};
+use std::time::{Duration, Instant, SystemTime};
 
+use dashmap::{DashMap, DashSet, Entry};
+use prost_types::{Timestamp, TimestampError};
 use proto::master_chunkserver::heartbeat_server::Heartbeat;
 use proto::master_chunkserver::{
     AssignLeaseRequest, AssignLeaseResponse, CollectStatusRequest, CollectStatusResponse,
@@ -10,40 +13,62 @@ use tonic::{Request, Response, async_trait};
 
 use crate::config::Config;
 
+static LEASE_DURATION: Duration = Duration::from_secs(60);
+
 pub struct Lease {
     pub expiration: Instant,
 }
 
 impl Lease {
-    pub fn new(expiration: Instant) -> Self {
-        Self { expiration }
-    }
-
     pub fn is_expired(&self) -> bool {
         return Instant::now() > self.expiration;
     }
+}
 
-    pub fn renew(&mut self) {
-        todo!();
+impl TryFrom<Timestamp> for Lease {
+    type Error = TimestampError;
+
+    fn try_from(timestamp: Timestamp) -> Result<Self, Self::Error> {
+        let systime = SystemTime::try_from(timestamp)?;
+
+        match systime.duration_since(SystemTime::now()) {
+            Ok(duration) => Ok(Lease {
+                expiration: Instant::now() + duration,
+            }),
+            Err(_) => Ok(Lease {
+                expiration: Instant::now(),
+            }),
+        }
     }
 }
 
 pub struct Chunkserver {
     chunk_dir: PathBuf,
-    chunks: HashSet<u64>,
-    active_leases: HashMap<u64>,
-}
-
-pub struct Chunk {
-    handle: u64,
-    path: PathBuf,
+    chunks: DashSet<u64>,
+    active_leases: DashMap<u64, Lease>,
 }
 
 impl Chunkserver {
-    pub fn new(config: Config, handles: HashSet<u64>) -> Self {
+    pub fn new(config: Config, chunks: DashSet<u64>) -> Self {
         let Config { chunk_dir } = config;
 
-        Self { chunk_dir, handles }
+        Self {
+            chunk_dir,
+            chunks,
+            active_leases: DashMap::new(),
+        }
+    }
+
+    fn create_chunk(&self, handle: u64) {
+        let path = self.chunk_dir.join(handle.to_string());
+
+        if let Err(e) = File::create_new(path) {
+            if e.kind() != std::io::ErrorKind::AlreadyExists {
+                panic!("Failed to create chunk {handle}: {e}");
+            }
+        }
+
+        self.chunks.insert(handle);
     }
 }
 
@@ -60,8 +85,25 @@ impl Heartbeat for Chunkserver {
         &self,
         request: Request<AssignLeaseRequest>,
     ) -> Result<Response<AssignLeaseResponse>, tonic::Status> {
-        let AssignLeaseRequest { handles } = request.into_inner();
+        let AssignLeaseRequest {
+            handles,
+            expirations,
+        } = request.into_inner();
 
-        todo!();
+        if handles.len() != expirations.len() {
+            return Ok(Response::new(AssignLeaseResponse { success: false }));
+        }
+
+        for (handle, expiration) in handles.into_iter().zip(expirations) {
+            self.create_chunk(handle);
+
+            let lease = Lease::try_from(expiration).map_err(|_| {
+                tonic::Status::failed_precondition("Timestamp already expired")
+            })?;
+
+            self.active_leases.insert(handle, lease);
+        }
+
+        Ok(Response::new(AssignLeaseResponse { success: true }))
     }
 }
